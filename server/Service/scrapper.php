cc2807dcaf0d1562e69c4934cb0c7219ead199c4
Joseph Benoy <josephbenoy03@gmail.com>
2021-10-07 19:17:29 +0530

    created scrapper class

---
+++ server/Service/scrapper.php
@@ -0,0 +1,7 @@
+<?php
+       require_once "./crawler/amazon.crawler.php";
+       class Scrapper extends Amazon{
+              public function getProductDetails(){
+                     return "Product details";
+              }
+       }
