9ce42bd6c5740cfa42e7570af6d7bf33864484ed
joseph-benoy <josephbenoy03@gmail.com>
2021-12-04 11:38:13 +0530

    added review controller

--- server/index.php
+++ server/index.php
@@ -2,6 +2,7 @@
 ob_start();
 require_once "./Controller/ProductController.php";
 require_once "./Controller/UserController.php";
+require_once "./Controller/ReviewController.php";
 require_once "./inc/bootstrap.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
@@ -13,5 +14,9 @@ if($uri[1]=="product"){
 else if($uri[1]=="user"){
        $userController = new UserController();
        $userController->{$uri[2]}();
+}
+else if($uri[1]=="review"){
+       $reviewController = new ReviewController();
+       $reviewController->{$uri[2]}();
 }
 ob_end_flush();
