789f97b6047c92519ee2b0e418621a8d46ce42f8
joseph-benoy <josephbenoy03@gmail.com>
2021-12-04 12:59:51 +0530

    added prodcut fetch endpoint

--- server/Model/Product.php
+++ server/Model/Product.php
@@ -1,5 +1,6 @@
 <?php
-       class Product{
+require_once "Database.php";
+       class Product extends Database{
               public function getProductSummary($productName){
                      return (new Amazon())->getProductSummary($productName);
               }
@@ -13,5 +14,22 @@
               }
               public function getFlipkart($productName){
                      return (new Flipkart())->getProductDetails($productName);
+              }
+              public function save($data){
+                     if($this->insert("INSERT INTO PRODUCT(NAME,URL) VALUES(?,?)",array_values($data),"ss")){
+                            return array("success"=>"inserted product successfully");
+                     }
+                     else{
+                            return array("error"=>"falied to insert");
+                     }
+              }
+              public function getProduct($data){
+                     $productData = $this->select("SELECT * FROM PRODUCT WHERE NAME=? OR URL=?",array_values($data),"ss");
+                     if($productData!=false){
+                            return array("success"=>$productData);
+                     }
+                     else{
+                            return array("error"=>"falied to fetch product");
+                     }
               }
        }
