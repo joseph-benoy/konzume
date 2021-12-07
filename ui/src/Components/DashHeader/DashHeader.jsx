ae66a2e62ed569070ede3f6abaf63b98440d331f
joseph-benoy <josephbenoy03@gmail.com>
2021-12-07 12:52:17 +0530

    logout configured

--- ui/src/Components/DashHeader/DashHeader.jsx
+++ ui/src/Components/DashHeader/DashHeader.jsx
@@ -1,7 +1,9 @@
 import './DashHeader.scss';
 import {Navbar,Container,Offcanvas,Nav,Form,FormControl,Button,NavDropdown} from 'react-bootstrap';
 import {Search,PersonBadge,DoorOpen} from 'react-bootstrap-icons';
+import { useNavigate } from 'react-router-dom';
 const DashHeader = ()=>{
+    const nav = useNavigate();
     return (
         <>
         <Navbar bg="dark" variant="dark">
@@ -10,7 +12,7 @@ const DashHeader = ()=>{
                 <Nav className="me-auto">
                     <Nav.Link href="#home" className="nav-links"><Search/></Nav.Link>
                     <Nav.Link href="#pricing"><PersonBadge/></Nav.Link>
-                    <Nav.Link href="#logout"><DoorOpen/></Nav.Link>
+                    <Nav.Link onClick={()=>{nav("/login");sessionStorage.removeItem("jwt")}}><DoorOpen/></Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
