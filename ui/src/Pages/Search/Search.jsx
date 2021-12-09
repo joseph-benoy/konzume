e6307b9071f5a4a2bf79a52546559171442f136d
joseph-benoy <josephbenoy@gmail.com>
2021-12-09 23:59:06 +0530

    changes to search

--- ui/src/Pages/Search/Search.jsx
+++ ui/src/Pages/Search/Search.jsx
@@ -1,6 +1,30 @@
 import { Col, Container, Form, Row,Button } from 'react-bootstrap';
 import './Search.scss';
+import React from 'react';
+import axios from 'axios';
 const Search = ()=>{
+    const [search,setSearch] = React.useState("");
+    const submitSearch = async()=>{
+        if(search!=''){
+            try{
+                const res = await axios({
+                    method: 'GET',
+                    url: '/product/searchproduct',
+                    params:{
+                        p:search
+                    }
+                });
+                console.log(res.data);
+                document.getElementById('search-image').style.display = "none";
+            }
+            catch(e){
+                console.log(e.response.data);
+            }
+        }
+        else{
+
+        }
+    }
     return(
         <Container fluid>
             <Row>
@@ -10,14 +34,14 @@ const Search = ()=>{
             </Row>
             <Row id="search-bar">
                 <Col lg={11}>
-                    <Form.Control type="search" size="large" type="text" placeholder="Search the product name"/>
+                    <Form.Control value={search} onChange={(e)=>{setSearch(e.target.value)}} type="search" size="large" type="text" placeholder="Search the product name"/>
                 </Col>
                 <Col lg={1}>
-                    <Button variant="dark">Search</Button>
+                    <Button onClick={submitSearch} variant="dark">Search</Button>
                 </Col>
             </Row>
             <Row>
-                <Col lg={5}>
+                <Col lg={5} id="search-image">
                     <img src="search-image.png" alt="search-image" id="search-image" fluid/>
                 </Col>
             </Row>
